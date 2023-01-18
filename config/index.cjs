41ebf55c3934ad49b6236d97d1eec69faf6978d5
Yacheng Li <1712670169@qq.com>
2023-01-18 11:22:00 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -47,8 +47,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
-
+  CALLBACK_TEMPLATE_ID: 'R8UumKrthVSMkfxRHyCGJaqc6G3hDQ2gYsINRdOXQNM',
   CALLBACK_USERS: [
     {
       name: '自己',
